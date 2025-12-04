6c6839e0d2c406df914cdaaee313fd912ad45255
yousrabenaissa43 <yousrabenaissa43@gmail.com>
2025-12-04 22:45:12 +0100

    minor fixes

--- spark-streaming.py
+++ spark-streaming.py
@@ -1,11 +1,12 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, avg
 from pyspark.sql.types import StructType, StructField, StringType, FloatType
 
 # Initialize SparkSession
 spark = SparkSession.builder \
     .appName("SeattleWeatherStreaming") \
     .getOrCreate()
+
 
 # Define schema of the incoming JSON
 schema = StructType([
@@ -32,16 +33,20 @@ json_df = df.selectExpr("CAST(value AS STRING) as json_str")
 parsed_df = json_df.select(from_json(col("json_str"), schema).alias("data")).select("data.*")
 
 # Example: Calculate average temp_max and temp_min per weather type (streaming aggregation)
-agg_df = parsed_df.groupBy("weather").avg("temp_max", "temp_min")
+agg_df = parsed_df.groupBy("weather").agg(
+    avg("temp_max").alias("avg_temp_max"),
+    avg("temp_min").alias("avg_temp_min")
+)
 
 # Write results to PostgreSQL
 def foreach_batch_function(batch_df, batch_id):
     batch_df.write \
         .format("jdbc") \
-        .option("url", "jdbc:postgresql://postgres-db:5432/weather_db") \
+        .option("url", "jdbc:postgresql://postgres-db:5432/temp_db") \
         .option("dbtable", "weather_summary") \
         .option("user", "postgres") \
         .option("password", "passw0rd") \
+        .option("driver", "org.postgresql.Driver") \
         .mode("append") \
         .save()
 
